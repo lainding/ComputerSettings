4b3b962558dab795f78ddb01c06a2b260f15e997
David <wding.cn@gmail.com>
2016-05-03 20:58:17 +0800

    daily commit

--- vim/updateplugin.cmd
+++ vim/updateplugin.cmd
@@ -1,3 +1,4 @@
-* update pathogen.vim
-curl -LSso "./vimfile/autoload/pathogen.vim" https://tpo.pe/pathogen.vim
-
+REM update pathogen.vim
+curl -LSso "./vimfiles/autoload/pathogen.vim" https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+REM update NERD_tree.vim
+curl -LSso "./vimfiles/bundle/nerdtree/plugin/NERD_tree.vim" https://raw.githubusercontent.com/scrooloose/nerdtree/master/plugin/NERD_tree.vim
